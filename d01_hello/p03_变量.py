9cd49c495adb987a844dc491c61b86f9ace29f26
lwq <2065277594@qq.com>
2022-12-01 17:40:22 +0800

    2022/12/1 17:40留念

---
+++ d01_hello/p03_变量.py
@@ -0,0 +1,12 @@
+money=11000
+#注意字符串拼接的方式
+print("工资才",money,"我真服了") 
+
+#查看数据类型 
+print(type(money))
+
+#数据类型转换
+money_str=str(money)
+print(type(money),money)
+print(type(money_str),money_str)
+#命名规范是下划线不是小驼峰
