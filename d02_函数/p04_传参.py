9d4ed5fc5dbc4909dcc1125961a702d4c8ca4b1f
lwq <2065277594@qq.com>
2022-12-15 20:34:00 +0800

    1

--- d02_函数/p04_传参.py
+++ d02_函数/p04_传参.py
@@ -1,4 +1,37 @@
-#1.根据参数位置
-#2.关键字参数(在调用时通过 k=v的方式)
-#3.默认参数
-#4.不定长参数*xx(将多个参数收集成一个元组),如果是**xx则需要使用k=v的方式(函数会将参数收集成一个字典)
+"""
+演示多种传参的形式
+"""
+
+
+def user_info(name, age, gender):
+    print(f"姓名是:{name}, 年龄是:{age}, 性别是:{gender}")
+
+
+# 按照位置
+user_info('小明', 20, '男')
+
+# 关键字参数
+user_info(name='冰冰', age=18, gender='女')
+user_info(age=17, gender='女', name='潇潇')    # 可以不按照参数的定义顺序传参
+user_info('甜甜', gender='女', age=19)
+
+
+# 缺省参数（默认值）需要放在最后面
+def user_info(name, age, gender,country="中国"):
+    print(f"姓名是:{name}, 年龄是:{age}, 性别是:{gender},国家是:{country}")
+user_info('小天', 13, '男')
+user_info('布鲁斯', 13, '男',"外国")
+
+
+# 不定长 - 位置不定长, *号
+# 不定长定义的形式参数会作为元组
+def user_info(*args):
+    print(f"args参数的类型是：{type(args)}，内容是:{args}")
+user_info(1, 2, 3, '小明', '男孩')
+
+
+# 不定长 - 关键字不定长, **号
+## 不定长定义的形式参数会作为字典
+def user_info(**kwargs):
+    print(f"args参数的类型是：{type(kwargs)}，内容是:{kwargs}")
+user_info(name='小王', age=11, gender='男孩')
---
+++ d07_调库实现图表功能/p01_折线图.py
@@ -0,0 +1,22 @@
+#为什么安装这个的时候不需要设置清华大学那个网址了?
+#引入折现图
+from pyecharts.charts import Line
+#引入标题设置
+from pyecharts.options import TitleOpts
+
+
+line=Line()
+#x轴
+line.add_xaxis(["2018","2019","2020"])
+#y轴
+line.add_yaxis("收入",[-1700,-1600,-1500])
+line.add_yaxis("卧推重量",[20,40,60,80])
+
+#设置图表全局选项
+line.set_global_opts(
+    #设置标题
+    title_opts=TitleOpts(title="世界经济格局doge",pos_left="center",pos_bottom="1%")
+)
+
+#生成图像
+line.render()
