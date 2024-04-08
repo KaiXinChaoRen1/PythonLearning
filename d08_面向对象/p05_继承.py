2d37f9c066f6d86f6ad7abdf7fbbaffeb26ee7b9
teng_xun_yun <teng_xun_yun@hehe.com>
2024-04-08 17:49:05 +0800

    1

--- d08_面向对象/p05_继承.py
+++ d08_面向对象/p05_继承.py
@@ -2,23 +2,32 @@ class Kaiyin:
     xueliang=100
     gongji=100
     
-    def dazhao():
+    def dazhao(self):
         print("凯隐大招")
 
     def __init__(self):
-        
         print("一个普通凯隐")
-#(里是父类)
-class KaiyinBlue(Kaiyin):
+
+class Nanju:
+    def zhufu(self):
+        print("男爵buff")
+    
+    def __init__(self):
+        print("一个奥恩")
+
+#(里是父类)  可以多继承
+
+class KaiyinBlue(Kaiyin,Nanju):
   
     
-    def dazhao():
+    def dazhao(self):
         print("蓝凯隐大招")
 
     def __init__(self):
         
         print("一个蓝色凯隐")
         
-hero=KaiyinBlue
+hero=KaiyinBlue()
 hero.dazhao() 
+hero.zhufu()
 print(hero.xueliang)
