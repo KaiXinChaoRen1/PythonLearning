f40c1b29f330f1683e2665eda2fa4a7331b01309
teng_xun_yun <teng_xun_yun@hehe.com>
2024-04-09 21:05:35 +0800

    2

--- d12_PySpark/p04_RDDreduceByKey.py
+++ d12_PySpark/p04_RDDreduceByKey.py
@@ -6,8 +6,10 @@ os.environ['PYSPARK_PYTHON'] = "/home/adev/PythonLearning/.venv/bin/python"
 conf = SparkConf().setMaster("local[*]").setAppName("reduceByKey_test")
 sc = SparkContext(conf=conf)
  
-# 准备一个RDD
+
 rdd = sc.parallelize([('男', 99), ('男', 88), ('女', 99), ('女', 66)])
+
 # 求男生和呃女生两个组的成绩之和
 rdd1 = rdd.reduceByKey(lambda a, b: a + b)
+
 print(rdd1.collect())
